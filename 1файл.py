bdce823f01ca5b45c8cd4c720b9bed9a4e5f3a04
lypers02 <90241550+lypers02@users.noreply.github.com>
2021-09-09 21:24:00 +0500

    Создала программу.
    Создала программу на Python которая запрашивает два числа, а затем выводит их сумму, разность и среднее арифметическое.

---
+++ 1файл.py
@@ -0,0 +1,8 @@
+a = float(input ("введите a = "))
+b = float(input ("введите b = "))
+s = a+b
+v = a-b
+ar = (a+b)/2
+print ("сумма равна = ", s)
+print ("разность равна = ", v)
+print ("среднее арифметическое = ", ar)
